543c7627522c01a494003dcd91bccdccdeb18324
ILYAS ZIYAOGLU <ilyas.ziyaoglu@consultant.turkcell.com>
2019-11-09 16:00:40 +0300

    old entities.deleted

--- src/main/java/exam/client/catalog/dto/response/document/DocumentResponse.java
+++
@@ -1,8 +0,0 @@
-package exam.client.catalog.dto.response.document;
-
-import exam.client.common.dto.response.AbstractResponse;
-
-public class DocumentResponse extends AbstractResponse {
-
-    private String url;
-}
